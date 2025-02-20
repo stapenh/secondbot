e3e29c378dca174b8de538da114a79c65147cb0c
Степан Мулин <stepan@MacBook-Pro-Stepan-2.local>
2025-02-21 01:43:57 +0300

    pls

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 API_TOKEN = "7053839067:AAF6eUYIcKIXcNIdC0fGCnErFbsgwnOoS2c"  # Токен вашего Telegram бота
 NOCODB_BASE_URL = "http://localhost:8080"  # Базовый URL NocoDB
-NOCODB_API_TOKEN = "YcMwNErI54Q5_Dp5H07YTE6H0WDAv2Vsw_0YX35U"  # API-токен NocoDB
+NOCODB_API_TOKEN = "X9UI1RE9GoovWu6x0exHQTOnMbbY8C1c1yshAYU_"  # API-токен NocoDB
