e3376a5846dad89d415174db96d335a3aafffc97
stapen <stepanmulin@MacBook-Pro-stapen.local>
2025-03-03 23:54:12 -0800

    small mistake

--- config.py
+++ config.py
@@ -3,13 +3,10 @@ import os
 API_TOKEN = os.getenv("API_TOKEN")
 NOCODB_BASE_URL = os.getenv("NOCODB_BASE_URL")
 NOCODB_API_TOKEN = os.getenv("NOCODB_API_TOKEN")
-
-ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(",")))  # Конвертируем в список чисел
-
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = int(os.getenv("DB_PORT", "5432"))  # Преобразуем в число
-
+DB_PORT = os.getenv("DB_PORT")
 INVITE_URL = os.getenv("INVITE_URL")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
